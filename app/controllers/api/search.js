30dd7585129a10a0a345e08162235f1b7d073459
Taewoo Kim <xodn4195@gmail.com>
2016-11-24 03:26:12 +0900

    Add elastic search querying

--- app/controllers/api/search.js
+++ app/controllers/api/search.js
@@ -1,30 +1,49 @@
 const
     express     = require('express'),
+    request     = require('request'),
     router      = express.Router({mergeParams: true});
 
 module.exports = router;
 
 router.get('/text', function(req, res) {
-    const {text} = req.query;
+    let {text} = req.query;
 
-    const result = [
-        {
-            _id: 1,
-            url: 'http://test.com/test',
-            title: 'test 1',
-            description: 'description 1'
-        },
-        {
-            _id: 2,
-            url: 'http://test.com/test2',
-            title: 'test 2',
-            description: 'description 2'
-        }
-    ];
+    const url = `http://localhost:9200/engine/formula/_search?q=${text}`;
 
-    res.send({
-        result
+    request(url, function(error, response, body) {
+        const result = body.hits.hits.map(function(item) {
+            return {
+                _id: item._id,
+                ...item._source
+            };
+        });
+
+        res.send({result});
     });
+    //
+    // res.send({
+    //     result: []
+    // });
+
+    //
+    // const result = [
+    //     {
+    //         _id: 1,
+    //         url: 'http://test.com/test',
+    //         title: 'test 1',
+    //         description: 'description 1'
+    //     },
+    //     {
+    //         _id: 2,
+    //         url: 'http://test.com/test2',
+    //         title: 'test 2',
+    //         description: 'description 2'
+    //     }
+    // ];
+    //
+    // res.send({
+    //     result
+    // });
 });
 
 router.get('/formulaImage', function(req, res, next) {
